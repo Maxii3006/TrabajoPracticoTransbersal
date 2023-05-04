6fe2b1760401f69d0f364255bdae753fa8a9ec40
Maxii3006 <129993260+Maxii3006@users.noreply.github.com>
2023-05-04 15:01:56 -0300

    Add files via upload

---
+++ TADlista.c
@@ -0,0 +1,165 @@
+#include "TADlista.h"
+
+/*void inicializa(TRconjunto * c){
+	c->tam=-1;
+}*/
+
+Tptrnodo crea_nodo(){
+	Tptrnodo nvo;
+	nvo=Tptrnodo(malloc(sizeof(Tnodo)));
+	nvo->dato=elem;
+	nvo->sig=NULL;
+	return nvo;
+}
+Tptrnodo agrega_elemento(char * elem){
+	
+}
+		
+/*void agrega_elemento(TRconjunto* C, char* token)
+{
+	C->tam++;
+	C->lis[C->tam]=token;
+}*/
+
+int pertenece(Tvec vec, int tam, char* cad)
+{
+	int i=0;
+	while(i<tam && strcmp(vec[i],cad)!=0){
+		i++;
+	}
+	if(i<tam){
+		return 1;
+	}
+	else return 0;
+	
+}
+
+TRconjunto crear_conj(char *cad){
+	
+	Tptrnodo lista=NULL;
+	lista=crea_nodo(strtok(cad, "{},"))
+	
+	if(token!=NULL)
+	{
+		agrega_elemento(&nvo, token);
+	}
+	while(token!=NULL)
+	{
+		token=strtok(NULL, "{},");
+		if(token!=NULL)
+			
+		{
+			if(pertenece(nvo.lis, nvo.tam, token)==0)
+				
+				agrega_elemento(&nvo, token);
+		}
+	}
+	
+	
+	return nvo;
+	
+}
+
+	
+	
+	
+	TRconjunto union_conj(TRconjunto C1, TRconjunto C2 )
+	{
+		TRconjunto C3;
+		int i;
+		C3=C1;
+		//C3.tam=-1;
+		
+		for(i=0; i<=C2.tam;i++)
+		{
+			if(pertenece(C3.lis, C3.tam, C2.lis[i])==0){
+				agrega_elemento(&C3, C2.lis[i]);
+			}
+			
+		}
+		
+		return C3;
+	}
+	
+ int cardinal_conj(TRconjunto A){
+	return A.tam;
+}
+	char* leeCadena(){
+		char* cadena=NULL;
+		void* aux;
+		int t=0;
+		char c;
+		
+		cadena=(char*) malloc(sizeof(char)); 
+		if (cadena==NULL) return cadena;
+		else {
+			c=getchar();
+			if (c!=EOF) { /*si el primer caracter no es fin de archivo*/
+				t=0;
+				if (c!='\n') {   //agregar el primer caracter leído solo si no es enter
+					cadena[t]=c; //equivalente *cadena=c
+					t++;
+				}	
+				/*LEER DEL SEGUNDO CARACTER EN ADELANTE*/
+				/*leer el resto de los caractaeres y agregarlos a la cadena*/
+				c=getchar();
+				while (c!=EOF && c!='\n') {
+					t++;
+					aux=(char*)realloc(cadena,sizeof(char)*t);
+					if (aux!=NULL) {
+						cadena=aux;
+						cadena[t-1]=c;
+					} else break;
+					c=getchar();
+				}
+				/*agregar marca de fin de cadena*/
+				aux=(char*)realloc(cadena,sizeof(char)*(t+1));
+				if (aux!=NULL) {
+					cadena=aux;
+					cadena[t]='\0';
+				} else cadena[t-1]='\0';
+			} else {
+				/*si se lee el fin del archivo en el primer caracter, no hay cadena*/
+				/*solo poner la marca de fin de cadena*/
+				cadena[0]='\0';
+			}
+			return cadena;
+		}
+	}
+			
+			
+TRconjunto inter_conj(TRconjunto C1, TRconjunto C2)
+{
+	TRconjunto C3;
+	//C3.tam=-1;
+	inicializa(&C3);
+	
+	int i;
+	
+	for(i=0; i<=C2.tam; i++)
+	{
+		if(pertenece(C1.lis, C1.tam, C2.lis[i])==0)
+		{
+			agrega_elemento(&C3, C2.lis[i]);
+		}
+	}
+	
+	return C3;	
+}
+
+void mostrar_conj(TRconjunto C)
+{
+	int i;
+	printf("\nConjunto resultante: \n");
+	printf("\n{");
+	for(i=0; i<=C.tam-1; i++)
+	{
+		printf("%s, ", C.lis[i]);
+	}
+	printf("%s}\n",C.lis[C.tam]);
+	
+}
+
+
+
+
---
+++ main.c
@@ -0,0 +1,64 @@
+/*INTEGRANTES:
+Pastrana, Oscar Tomas Francisco
+Romero, Franco Matias
+Ricalde, Maximiliano Jeremias*/
+
+#include "TADlista.h"
+
+int menu();
+
+int main(int argc, char *argv[]) {
+	char* cad; //"{1,2,3,4}" "{uno,dos}"
+	int op;
+	TRconjunto C1, C2, C3;
+	do{
+		op = menu();
+		switch(op){
+		case 0:
+			break;
+		case 1:
+			fflush(stdin);
+			cad=leeCadena();
+			C1=crear_conj(cad);
+			mostrar_conj(C1);
+			break;
+		case 2:	
+			fflush(stdin);
+			cad=leeCadena();
+			C2=crear_conj(cad);
+			C3=union_conj(C1,C2);
+			mostrar_conj(C3);
+			break;
+		case 3:
+			fflush(stdin);
+			cad=leeCadena();
+			C2=crear_conj(cad);
+			C3=inter_conj(C1,C2);
+			mostrar_conj(C3);
+			break;
+		case 4:
+			//printf("\nQue conjunto desea mostrar? ");
+			//scanf("%d", &valor);
+			//switch(valor){
+				//como hago pra saber cuantos conjuntos hay? si agrego tre conjuntos no hay drama pero si agrego muchos el switch no me sirve
+			//}
+			mostrar_conj(C1);//pusimos c1 para saber si funcionaba nomas
+			break;
+		default:
+			printf("\nOpcion no valida");
+		}
+	} while(op!=0);
+	return 0;
+}
+
+
+int menu(){
+	int op;
+	
+	printf("\nMenu de opciones \n");
+	printf("\n 0: Salir del Programa; \n 1: Ingresar un Conjunto; \n 2: Union de Conjuntos;\n 3: Intersección de Conjuntos;\n 4: Mostrar un Conjunto;\n\n");
+	printf("Seleccione su opcion: ");
+	scanf("%d", &op);
+	
+	return op;
+}
